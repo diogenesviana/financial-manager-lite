42aab655a3b24324c6d6de30be905d34330350fe
Diógenes Viana <65191714+diogenesviana@users.noreply.github.com>
2026-07-01 15:29:37 -0300

    fix: ajustado zIndex do QrCodeScanner para ficar na frente do modal de novo gasto

--- src/components/QrCodeScanner.tsx
+++ src/components/QrCodeScanner.tsx
@@ -90,7 +90,7 @@ export const QrCodeScanner: React.FC<QrCodeScannerProps> = ({ onScanSuccess, onC
       right: 0,
       bottom: 0,
       backgroundColor: 'rgba(0, 0, 0, 0.85)',
-      zIndex: 2000,
+      zIndex: 100000,
       display: 'flex',
       flexDirection: 'column',
       alignItems: 'center',
